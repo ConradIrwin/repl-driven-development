9d4f612c3dfa1f8ae7e990eae0a606f20bea4761
Conrad Irwin <conrad.irwin@gmail.com>
2013-11-06 01:39:58 -0800

    Fix all the things

--- mongo.rb
+++ mongo.rb
@@ -3,6 +3,6 @@ require 'pry'
 def find_user(first, last)
   names = [first, last]
   binding.pry
-  names << first + " " + last
+  names << first + last
   $mongo[:users].where(name: {"$in" => names}).to_a
 end
