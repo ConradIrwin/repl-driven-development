6740ee0a8f8b43a0a3ee3c8b084f3fa512ba3db5
Conrad Irwin <conrad.irwin@gmail.com>
2013-11-05 23:51:12 -0800

    Commit style changes

--- mongo.rb
+++ mongo.rb
@@ -1,6 +1,8 @@
 require './initializer'
+require 'pry'
 def find_user(first, last)
   names = [first, last]
-  names << first + last
+  binding.pry
+  names << first + " " + last
   $mongo[:users].where(name: {"$in" => names}).to_a
 end
