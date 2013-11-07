9894ecf2c543f849fc0162e73cc0132d7710e11d
Conrad Irwin <conrad.irwin@gmail.com>
2013-11-07 06:48:09 -0800

    remove binding.pry

--- mongo.rb
+++ mongo.rb
@@ -2,7 +2,6 @@ require './initializer'
 require 'pry'
 def find_user(first, last)
   names = [first, last]
-  binding.pry
   names << first + last
   $mongo[:users].where(name: {"$in" => names}).to_a
 end
